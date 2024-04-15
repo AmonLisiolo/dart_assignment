5751e7587e0df9ac2487496445e984af3a36bfa4
AmonLisiolo <amon.lisiolo@gmail.com>
2024-04-15 15:07:55 +0300

    main

---
+++ advanced_oop.dart
@@ -0,0 +1,56 @@
+class Student {
+  String studentName;
+  int age;
+  String grade;
+
+  Student(this.studentName, this.age, this.grade);
+
+  void displayInfo() {
+    print("Student Name: $studentName");
+    print("Student Age: $age");
+    print("Student Grade: $grade");
+  }
+}
+
+class Teacher {
+  String name;
+  int age;
+  String subject;
+
+  Teacher(this.name, this.age, this.subject);
+
+  void displayInfo() {
+    print("Teacher's Name: $name");
+    print("Teache's Age: $age");
+    print("Subject: $subject");
+  }
+}
+
+class SubjectDetails {
+  String subject1;
+  int duration;
+  Student student;
+  Teacher teacher;
+
+  // Constructor for SubjectDetails
+  SubjectDetails(this.subject1, this.duration, String studentName, int age,
+      String grade, String teacherName)
+      : student =
+            Student(studentName, age, grade), // Calling Student constructor
+        teacher =
+            Teacher(teacherName, age, subject1); // Creating Teacher object
+
+  // Property to hold the Teacher object
+
+  void displayInfo() {
+    student.displayInfo(); // Call displayInfo method from Student
+    teacher.displayInfo(); // Call displayInfo method from Teacher
+    print("Subject: $subject1");
+    print("Duration: $duration");
+  }
+}
+
+void main() {
+  var subject = SubjectDetails("Math", 60, "Alice", 30, "A", "Mr. Smith");
+  subject.displayInfo();
+}
